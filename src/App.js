ba3f4b198a4510d46a386e860c84c86fab5091a2
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-01 00:13:01 -0500

    fixed giphy async function call to save the gif and update state of characters list

--- src/App.js
+++ src/App.js
@@ -2,53 +2,67 @@ import React from "react";
 import './App.css';
 
 import { CardList } from './components/card-list/card-list.component'
-
-import { GiphyFetch } from '@giphy/js-fetch-api';
+import {GiphyFetch} from "@giphy/js-fetch-api";
 
 class App extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            characters: [
-                {
-                    id: 'char1',
-                    name: 'Superman'
-                },
-                {
-                    id: 'char2',
-                    name: 'Batman'
-                },
-                {
-                    id: 'char3',
-                    name: 'Spiderman'
-                },
-                {
-                    id: 'char4',
-                    name: 'Wonder Woman'
-                },
-                {
-                    id: 'char5',
-                    name: 'Joker'
-                },
-                {
-                    id: 'char6',
-                    name: 'Wolverine'
-                }
-            ]
+            characters: []
         }
     }
 
     componentDidMount() {
         const gf = new GiphyFetch('OhyKGDrCTct0EKGl1NmagjvwwelQR6WG')
-        this.state.characters.map(character => {
-            return gf.search(character.name,{ sort: 'relevant', limit: 1 }).then((gifs) => {
-                this.state.characters.map(character => {
-                    return character.url = gifs.data[0].url
-                })
-                console.log(gifs.data[0].url);
-            });
-        });
+        let characters = [
+            {
+                id: 'char1',
+                name: 'Superman',
+                url: ''
+            },
+            {
+                id: 'char2',
+                name: 'Batman',
+                url: ''
+            },
+            {
+                id: 'char3',
+                name: 'Spiderman',
+                url: ''
+            },
+            {
+                id: 'char4',
+                name: 'Wonder Woman',
+                url: ''
+            },
+            {
+                id: 'char5',
+                name: 'Joker',
+                url: ''
+            },
+            {
+                id: 'char6',
+                name: 'Wolverine',
+                url: ''
+            }
+        ]
+
+        let addGif = async () => {
+            for (const character of characters) {
+                await gf.search(character.name,{ sort: 'relevant', limit: 1 }).then(gifs => {
+                    character.url = gifs.data[0].embed_url;
+                });
+            }
+
+            console.log('State Change')
+            await this.setState({
+                characters: characters
+            })
+            console.log(this.state.characters)
+        };
+
+        addGif();
     }
 
     render() {
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,6 +5,8 @@ import './card.css'
 export const Card = props => {
     return (
         <div className={'card-container'}>
+            <iframe src={props.character.url} width="480" height="360" frameBorder="0"
+                    className="giphy-embed" allowFullScreen></iframe>
             {/*<img crossOrigin={'anonymous'} src={props.character.url} alt="character" />*/}
             <h3> {props.character.name} </h3>
         </div>
