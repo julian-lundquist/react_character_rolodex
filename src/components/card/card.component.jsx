ab2f6127a811d25faba1983301806f3e8ee62c0e
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-02 20:44:08 -0500

    added card list centering and improved responsive view

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,8 +5,8 @@ import './card.css'
 export const Card = props => {
     return (
         <div className={'card-container'}>
-            <iframe src={props.character.url} title={props.character.name} width="100%" height="280px" frameBorder="0"
-                    className="giphy-embed" allowFullScreen></iframe>
+            <iframe src={props.character.url} title={props.character.name} frameBorder="0"
+                    className={'giphy-embed'} allowFullScreen></iframe>
             {/*<img crossOrigin={'anonymous'} src={props.character.url} alt="character" />*/}
             <h3> {props.character.name} </h3>
         </div>
