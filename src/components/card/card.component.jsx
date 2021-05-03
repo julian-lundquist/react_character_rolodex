0eda3423bfb7359c346e5c83c7877963c8e40355
julian-lundquist <julian.j.lundquist@gmail.com>
2021-05-02 19:28:25 -0500

    added the card list to be mobile responsive

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -5,7 +5,7 @@ import './card.css'
 export const Card = props => {
     return (
         <div className={'card-container'}>
-            <iframe src={props.character.url} width="" height="280" frameBorder="0"
+            <iframe src={props.character.url} title={props.character.name} width="100%" height="280px" frameBorder="0"
                     className="giphy-embed" allowFullScreen></iframe>
             {/*<img crossOrigin={'anonymous'} src={props.character.url} alt="character" />*/}
             <h3> {props.character.name} </h3>
